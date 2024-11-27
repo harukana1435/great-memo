dfd94ced9034590d2bd605879e4d95ab2fd737b1
harukana1435 <harukana1435@gmail.com>
2024-11-27 13:44:10 +0900

    second commit

---
+++ api/notion.js
@@ -0,0 +1,31 @@
+// api/notion.js
+
+const { Client } = require("@notionhq/client");
+const dotenv = require("dotenv");
+
+// 環境変数を読み込む
+dotenv.config();
+
+// Notionクライアントを設定
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+module.exports = async (req, res) => {
+  if (req.method === "GET") {
+    try {
+      // Notionデータベースのプロパティを取得
+      const response = await notion.databases.retrieve({
+        database_id: process.env.NOTION_DATABASE_ID,
+      });
+      console.log(response);
+      // レスポンスを返す
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "Failed to retrieve Notion database properties" });
+    }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+};
