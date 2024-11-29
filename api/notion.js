7f02664739bd0028e124466d6a46f94df489b538
harukana1435 <harukana1435@gmail.com>
2024-11-29 13:14:59 +0900

    change prompt

--- api/notion.js
+++ api/notion.js
@@ -80,10 +80,10 @@ ${tabContent}
             .map((detail) => `- [${detail.title}](${detail.url})`)
             .join("\n") +
           "\nーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー\n" +
-          "## メモ\n" +
+          "## 目次\n" +
           results.tabContent +
           "\nーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー\n" +
-          "## 原文\n" +
+          "## メモ\n" +
           tabContent,
       );
       console.log(blocks);
