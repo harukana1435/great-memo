d7703f964ca0d37601ce3901d6090d871379bc95
harukana1435 <harukana1435@gmail.com>
2024-12-11 17:46:37 +0900

    syusei

--- api/notion.js
+++ api/notion.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
       const databaseId = process.env.NOTION_DATABASE_ID;
       console.log(tabContent);
 
-      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
       const prompt = `
 入力文で示される文章をNotionのMarkdown記法でまとめて、出力してください。
